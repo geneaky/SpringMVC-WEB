7cc4d9862349306273ddca0cc050a4c3b6fb1b80
geneaky <kaktus418@gmail.com>
2022-01-01 22:54:56 +0900

    [feat] item regist @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -38,9 +40,27 @@ public class BasicItemController {
     return "basic/addForm";
   }
 
+  /*@PostMapping("/add")
+  public String save(@RequestParam String itemName,@RequestParam Integer price, @RequestParam Integer quantity,Model model) {
+    Item item = new Item();
+    item.setItemName(itemName);
+    item.setPrice(price);
+    item.setQuantity(quantity);
+
+    itemRepository.save(item);
+
+    model.addAttribute("item",item);
+
+    return "basic/item";
+  }*/
+
   @PostMapping("/add")
-  public String save() {
-    return "basic/addForm";
+  public String save(@ModelAttribute("item") Item item) {
+
+    itemRepository.save(item);
+//    model.addAttribute("item",item); // 자동 추가, 생략 가능
+
+    return "basic/item";
   }
 
   @PostConstruct
